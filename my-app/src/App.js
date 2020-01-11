b97d6ccfa8a0611fe145255ef6cae1653854807c
mpapili <mpapili@bloomberg.net>
2020-01-10 23:12:01 -0500

    dynamic style, states, props, and events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,43 @@ import './App.css';
 
 
 class App extends Component {
+
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Take Out Trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'Learn React',
+                completed: true
+            },
+            {
+                id: 3,
+                title: "Wash the Dishes",
+                completed: false
+            }
+        ]
+    }
+
+    markComplete = (id) => {
+        this.setState({ todos: this.state.todos.map(todo => {
+            if(todo.id === id) {
+                    todo.completed = !todo.completed; // make it the opposite
+               }
+            return todo;
+        }) }); // ugly amount of closing brackets
+    }
+
     render() {
-  return (
-    <div className="App">
-        <h1> My App! </h1>
-        <Todos />
-    </div>
-  );
+        return (
+            <div className="App">
+                <h1> My App! </h1>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} />
+            </div>
+        );
         
     }
 }
